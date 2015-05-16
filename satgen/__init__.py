2a069c22f1a68ab34d0040cabe313c1b699a8b69
Andrei Vacariu <andrei@avacariu.me>
2015-05-16 19:38:55 +0100

    Add a very dumb uniform SAT generator

---
+++ satgen/__init__.py
@@ -0,0 +1,29 @@
+from enum import Enum
+import random
+
+class Distribution(Enum):
+    uniform = 0
+    powerlaw = 1
+
+class Instance:
+    def __init__(self, num_vars, num_clauses, k,
+            distribution=Distribution.uniform):
+        self.num_vars = num_vars
+        self.num_clauses = num_clauses
+        self.k = k
+        self.distribution = distribution
+
+        self.variables = range(1, num_vars+1)
+        self.clauses = {}
+
+    def generate(self):
+        if self.distribution is Distribution.uniform:
+            for m in range(self.num_clauses):
+                clause = random.sample(self.variables, self.k)
+                for i, var in enumerate(clause):
+                    clause[i] = var * (-1 ** random.randint(0, 1))
+
+                self.clauses[m] = clause
+        else:
+            # TODO
+            pass
---
+++ satgen/__main__.py
@@ -0,0 +1,10 @@
+n = 10
+m = 5
+k = 3
+
+from . import Instance, Distribution
+
+sat = Instance(n, m, k)
+sat.generate()
+
+print(sat.clauses)
