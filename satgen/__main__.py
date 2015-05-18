861924da2bb9aac8ec81b8ddd9ee8541ad26e134
Andrei Vacariu <andrei@avacariu.me>
2015-05-18 12:29:23 +0100

    Start using click for the CLI

--- satgen/__main__.py
+++ satgen/__main__.py
@@ -1,10 +1,18 @@
-n = 10
-m = 5
-k = 3
-
+import click
 from . import Instance, Distribution
 
-sat = Instance(n, m, k)
-sat.generate()
+@click.command()
+@click.option('--variables', '-n', default=5, type=int,
+        help='Number of variables.')
+@click.option('--clauses', '-m', default=3, type=int,
+        help='Number of clauses.')
+@click.option('--clause-size', '-k', default=3, type=int,
+        help='The size of each clause.')
+def main(variables, clauses, clause_size):
+    sat = Instance(variables, clauses, clause_size)
+    sat.generate()
 
-print(sat)
+    click.echo(str(sat))
+
+if __name__ == '__main__':
+    main()
